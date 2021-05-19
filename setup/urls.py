4e8f5b20473f0b807b3e0e11d04a5ea62d3fd03f
Pedro <pedromiguelnevesdemeu@gmail.com>
2021-05-19 15:34:14 -0300

    first-commit preparando ambiente

--- setup/urls.py
+++ setup/urls.py
@@ -1,18 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
 from rest_framework import routers
+from clientes.views import ClientesViewSet
 
 router = routers.DefaultRouter()
-router.register('alunos', AlunosViewSet, basename="Alunos")
-router.register('cursos', CursosViewSet, basename="Cursos")
-router.register('matriculas', MatriculasViewSet, basename="Matriculas")
-
+router.register('clientes', ClientesViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('aluno/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
-    path('curso/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view()),
-
 ]
